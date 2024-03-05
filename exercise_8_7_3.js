1188abc531d595102077a4c0dd28081ddc683230
RomanLugovskoy <romagma29@gmail.com>
2024-03-05 07:39:18 +0300

    ex 3 fix

--- exercise_8_7_3.js
+++ exercise_8_7_3.js
@@ -1,11 +1,11 @@
 'use strict'
 const users = [
-    { name: 'Алексей', age: 47, },
-    { name: 'Борис', age: 55, },
+	{ name: 'Алексей', age: 47, },
+	{ name: 'Борис', age: 55, },
 	{ name: 'Абдулло', age: 17, },
-    { name: 'Марина', age: 16, },
+	{ name: 'Марина', age: 16, },
 	{ name: 'Александра', age: 28, },
-    { name: 'Вова', age: 100, }
+	{ name: 'Вова', age: 100, }
 ];
 const adultUsers = users.filter(user => user['age'] >= 18);
 const userNames = users.map(user => user['name']);
